c28655973b9ff5020b93cb6e1420fe310c3e2591
Danilo <daniloucelo087@gmail.com>
2024-04-23 00:25:02 -0600

    Calcular viajes y combustible

---
+++ CalculodeViajes/CalculodeViajes/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace CalculodeViajes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.origen = new System.Windows.Forms.TextBox();
+            this.destino = new System.Windows.Forms.TextBox();
+            this.velocidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(323, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CALCULADORA DE VIAJES";
+            // 
+            // origen
+            // 
+            this.origen.BackColor = System.Drawing.Color.Cyan;
+            this.origen.ForeColor = System.Drawing.Color.Blue;
+            this.origen.Location = new System.Drawing.Point(336, 96);
+            this.origen.Name = "origen";
+            this.origen.Size = new System.Drawing.Size(100, 20);
+            this.origen.TabIndex = 1;
+            // 
+            // destino
+            // 
+            this.destino.BackColor = System.Drawing.Color.Cyan;
+            this.destino.ForeColor = System.Drawing.Color.Blue;
+            this.destino.Location = new System.Drawing.Point(336, 170);
+            this.destino.Name = "destino";
+            this.destino.Size = new System.Drawing.Size(100, 20);
+            this.destino.TabIndex = 2;
+            // 
+            // velocidad
+            // 
+            this.velocidad.BackColor = System.Drawing.Color.Cyan;
+            this.velocidad.ForeColor = System.Drawing.Color.Blue;
+            this.velocidad.Location = new System.Drawing.Point(336, 243);
+            this.velocidad.Name = "velocidad";
+            this.velocidad.Size = new System.Drawing.Size(100, 20);
+            this.velocidad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(222, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PUNTO DE ORIGEN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(222, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PUNTO DE DESTINO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Yellow;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(193, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "VELOCIDAD CONSTANTE";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(507, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.velocidad);
+            this.Controls.Add(this.destino);
+            this.Controls.Add(this.origen);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox origen;
+        private System.Windows.Forms.TextBox destino;
+        private System.Windows.Forms.TextBox velocidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
